831440b277ece16721e97b99f2a21cd67e592b62
gaelle <gaellebiz@gmail.com>
2021-05-26 19:20:12 +0200

    reinstall npm

---
+++ pages/main.js
@@ -0,0 +1,6 @@
+let addToCart = document.querySelectorAll ('.add__cart');
+for(let i = 0; i< add__cart.lenght; i++){
+    add__cart[i].addEventListener ("click"), () => {
+        numberOfItems();
+    };
+} 
